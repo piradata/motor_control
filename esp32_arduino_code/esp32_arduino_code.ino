c58acf7fd409ea4bffb03138e57bdcaaff3b2756
PIRADATA <guilhermefercar@gmail.com>
2019-03-10 02:12:36 -0300

    ha

--- esp32_arduino_code/esp32_arduino_code.ino
+++ esp32_arduino_code/esp32_arduino_code.ino
@@ -1,43 +1,55 @@
-/*
-  DigitalReadSerial
-
-  Reads a digital input on pin 2, prints the result to the Serial Monitor
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/DigitalReadSerial
-*/
-
-// digital pin 2 has a pushbutton attached to it. Give it a name:
 #define pinG 18
 #define pinR 19
 #define led 2
 #define pot 4
+#define freq 10000
+#define MA 22
+#define MB 23
+#define MAChannel  0
+#define MBChannel  1
+#define resolution  10 //PWM 0 -> 1023
 
-volatile int GG;
-volatile int RR;
+#define Kp 200
+#define Kd 1
+#define H_T 0.1
 
+volatile int GG, RR;
 volatile int CWcounter = 0;
- 
-portMUX_TYPE mux = portMUX_INITIALIZER_UNLOCKED;
 
+int erro, lasterro = 0;
+int POS, oldPOS = 0, VEL = 0;
+int REF, REF_F = 0;
+int out, OUTF = 0;
+int AUMENTA;
+
+//DONT TOUCH THIS SHIT!!! IT IS FUCKING WORKING!!!
+portMUX_TYPE mux = portMUX_INITIALIZER_UNLOCKED;
 void IRAM_ATTR handleInterruptG() {
   portENTER_CRITICAL_ISR(&mux);
-  if (digitalRead(pinG) != GG){
+  if (digitalRead(pinG) != GG) {
+    //(GG != oldGG)?(GG==RR?CWcounter--:CWcounter++):(GG==RR?CWcounter++:CWcounter--);
     GG = !GG;
     RR = digitalRead(pinR);
-    GG==RR?CWcounter--:CWcounter++;
-    }
+    GG == RR ? CWcounter-- : CWcounter++;
+  }
   portEXIT_CRITICAL_ISR(&mux);
 }
 
 void setup() {
   Serial.begin(115200);
 
+  ledcSetup(MAChannel, freq, resolution);
+  ledcAttachPin(MA, MAChannel);
+  ledcWrite(MAChannel, 0);
+
+  ledcSetup(MBChannel, freq, resolution);
+  ledcAttachPin(MB, MBChannel);
+  ledcWrite(MBChannel, 0);
+
   pinMode(pinG, INPUT);
   pinMode(pinR, INPUT);
   pinMode(led, OUTPUT);
-  
+
   GG = digitalRead(pinG);
   RR = digitalRead(pinR);
   Serial.print(GG);
@@ -45,38 +57,42 @@ void setup() {
   Serial.print(RR);
   Serial.print("/");
   Serial.println(CWcounter);
-  
+
   attachInterrupt(digitalPinToInterrupt(pinG), handleInterruptG, CHANGE);
-  
-  while(true) {
-    int atual = CWcounter>0?((CWcounter%11484)*360)/11484:((11484+(CWcounter%11484))*360)/11484;
-    int ideal = (analogRead(pot)*360)/4095;
-    int erro  = ideal>atual?((ideal-atual)<180?ideal-atual:-atual-360+ideal):((atual-ideal)>180?ideal+360-atual:-atual+ideal);
+
+  while (true) {
+    POS = CWcounter > 0 ? ((CWcounter % 11484) * 360) / 11484 : ((11484 + (CWcounter % 11484)) * 360) / 11484;
+    VEL = POS - oldPOS;
+    oldPOS = POS;
+
+    REF = (analogRead(pot) * 360) / 4095;
+    REF_F = 0.561 * REF_F + (1 - 0.561) * REF;
+
+    erro  = REF_F > POS ? ((REF_F - POS) < 180 ? REF_F - POS : -POS - 360 + REF_F) : ((POS - REF_F) > 180 ? REF_F + 360 - POS : -POS + REF_F);
+
+    out =  Kp * (erro - (Kd * VEL) / H_T);
+    OUTF = 0.96*OUTF + 0.04* out;
+    
+    if (OUTF >  1023) OUTF = 1023;
+    if (OUTF < -1023) OUTF = -1023;
+    
+    if (OUTF>0) {ledcWrite(MAChannel, 0);ledcWrite(MBChannel, OUTF);}
+    else {ledcWrite(MBChannel, 0);ledcWrite(MAChannel, -OUTF);}
+
     Serial.print(CWcounter);
     Serial.print("\t");
-    Serial.print(atual);
+    Serial.print(POS);
     Serial.print("\t");
-    Serial.print(ideal);
+    Serial.print(REF_F);
     Serial.print("\t");
-    Serial.println(erro);
-  }  
+    Serial.print(erro);
+    Serial.print("\t");
+    Serial.println(OUTF);
+
+    lasterro = erro;
+  }
 }
 
-void loop(){;}
-
-//if(oldCWcounter != CWcounter){
-//oldCWcounter = CWcounter;
-//(GG != oldGG)?(GG==RR?CWcounter--:CWcounter++):(GG==RR?CWcounter++:CWcounter--);
-
-//void IRAM_ATTR handleInterruptG() {
-//  digitalWrite(led, HIGH);
-//  GG = digitalRead(pinG);
-//  portENTER_CRITICAL_ISR(&mux);
-//  if (GG != oldGG){
-//    oldGG = GG;
-//    RR = digitalRead(pinR);
-//    GG==RR?CWcounter--:CWcounter++;
-//    }
-//  portEXIT_CRITICAL_ISR(&mux);
-//  digitalWrite(led, LOW);
-//}
+void loop() {
+  ;
+}
