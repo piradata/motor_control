b5c5407322644109ab32eec49effd92128dc433f
PIRADATA <guilhermefercar@gmail.com>
2019-03-12 06:42:26 -0300

    time interruption, Mn
    adicionada interrupção por tempo no codigo, falta testar, talvez nao funcione direito XD. bom lembrar de quando testar isso aumentarum pouco o magic number, reduçao de fsbrica pode estar errada.

--- esp32_arduino_code/esp32_arduino_code.ino
+++ esp32_arduino_code/esp32_arduino_code.ino
@@ -16,24 +16,33 @@
 
 volatile int GG, RR;
 volatile int CWcounter = 0;
+volatile int timerFlag = 0;
 
 int erro, lasterro = 0;
 int POS, oldPOS = 0, VEL = 0;
 int REF, REF_F = 0;
 int out, OUTF = 0;
-int AUMENTA;
+
+hw_timer_t * timer = NULL;
+portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
+
+void IRAM_ATTR onTimer() {
+  portENTER_CRITICAL_ISR(&timerMux);
+  timerFlag++;
+  portEXIT_CRITICAL_ISR(&timerMux);
+}
 
 //DONT TOUCH THIS SHIT!!! IT IS FUCKING WORKING!!!
 portMUX_TYPE mux = portMUX_INITIALIZER_UNLOCKED;
 void IRAM_ATTR handleInterruptG() {
-  portENTER_CRITICAL_ISR(&mux);
   if (digitalRead(pinG) != GG) {
     //(GG != oldGG)?(GG==RR?CWcounter--:CWcounter++):(GG==RR?CWcounter++:CWcounter--);
     GG = !GG;
     RR = digitalRead(pinR);
+    portENTER_CRITICAL_ISR(&mux);
     GG == RR ? CWcounter-- : CWcounter++;
+    portEXIT_CRITICAL_ISR(&mux);
   }
-  portEXIT_CRITICAL_ISR(&mux);
 }
 
 void setup() {
@@ -51,46 +60,67 @@ void setup() {
   pinMode(pinR, INPUT);
   pinMode(led, OUTPUT);
 
+  portENTER_CRITICAL_ISR(&mux);
   GG = digitalRead(pinG);
   RR = digitalRead(pinR);
+  portEXIT_CRITICAL_ISR(&mux);
+
   Serial.print(GG);
   Serial.print("/");
   Serial.print(RR);
   Serial.print("/");
   Serial.println(CWcounter);
 
+  timer = timerBegin(0, 80, true);
+  timerAttachInterrupt(timer, &onTimer, true);
+  timerAlarmWrite(timer, 100000, true);
+  timerAlarmEnable(timer);
+
   attachInterrupt(digitalPinToInterrupt(pinG), handleInterruptG, CHANGE);
 
   while (true) {
-    POS = CWcounter > 0 ? ((CWcounter % magic_N) * 360) / magic_N : ((magic_N + (CWcounter % magic_N)) * 360) / magic_N;
-    VEL = POS - oldPOS;
-    oldPOS = POS;
-
-    REF = (analogRead(pot) * 360) / 4095;
-    REF_F = 0.561 * REF_F + (1 - 0.561) * REF;
-
-    erro  = REF_F > POS ? ((REF_F - POS) < 180 ? REF_F - POS : -POS - 360 + REF_F) : ((POS - REF_F) > 180 ? REF_F + 360 - POS : -POS + REF_F);
-
-    out =  Kp * (erro - (Kd * VEL) / H_T);
-    OUTF = 0.96*OUTF + 0.04* out;
+    if (timerFlag > 0) {
     
-    if (OUTF >  1023) OUTF = 1023;
-    if (OUTF < -1023) OUTF = -1023;
-    
-    if (OUTF>0) {ledcWrite(MAChannel, 0);ledcWrite(MBChannel, OUTF);}
-    else {ledcWrite(MBChannel, 0);ledcWrite(MAChannel, -OUTF);}
+      portENTER_CRITICAL(&timerMux);
+      timerFlag--;
+      portEXIT_CRITICAL(&timerMux);
 
-    Serial.print(CWcounter);
-    Serial.print("\t");
-    Serial.print(POS);
-    Serial.print("\t");
-    Serial.print(REF_F);
-    Serial.print("\t");
-    Serial.print(erro);
-    Serial.print("\t");
-    Serial.println(OUTF);
+      POS = CWcounter > 0 ? ((CWcounter % magic_N) * 360) / magic_N : ((magic_N + (CWcounter % magic_N)) * 360) / magic_N;
+      VEL = POS - oldPOS;
+      oldPOS = POS;
 
-    lasterro = erro;
+      REF = (analogRead(pot) * 360) / 4095;
+      REF_F = 0.561 * REF_F + (1 - 0.561) * REF;
+
+      erro  = REF_F > POS ? ((REF_F - POS) < 180 ? REF_F - POS : -POS - 360 + REF_F) : ((POS - REF_F) > 180 ? REF_F + 360 - POS : -POS + REF_F);
+
+      out =  Kp * (erro - (Kd * VEL) / H_T);
+      OUTF = 0.96 * OUTF + 0.04 * out;
+
+      if (OUTF >  1023) OUTF = 1023;
+      if (OUTF < -1023) OUTF = -1023;
+
+      if (OUTF > 0) {
+        ledcWrite(MAChannel, 0);
+        ledcWrite(MBChannel, OUTF);
+      }
+      else {
+        ledcWrite(MBChannel, 0);
+        ledcWrite(MAChannel, -OUTF);
+      }
+
+      Serial.print(CWcounter);
+      Serial.print("\t");
+      Serial.print(POS);
+      Serial.print("\t");
+      Serial.print(REF_F);
+      Serial.print("\t");
+      Serial.print(erro);
+      Serial.print("\t");
+      Serial.println(OUTF);
+
+      lasterro = erro;
+    }
   }
 }
 
